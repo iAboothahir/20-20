defcf490c1618bb5bf151d329db3833f54b1bc4b
iAboothahir <65064906+iAboothahir@users.noreply.github.com>
2021-07-26 20:57:40 +0530

    panel-simple.c:add nt36672_txd simple panel

--- panel-simple.c
+++ panel-simple.c
@@ -4641,6 +4641,38 @@ static const struct panel_desc_dsi td4310_txd = {
 	.lanes = 4,
 };
 
+static const struct drm_display_mode nt36672_txd_mode = {
+	.clock = (1080 + 122 + 8 + 76) * (2160 + 20 + 4 + 28) * 60 / 1000,
+	.hdisplay = 1080,
+	.hsync_start = 1080 + 122,
+	.hsync_end = 1080 + 122 + 8,
+	.htotal = 1080 + 122 + 8 + 76,
+	.vdisplay = 2160,
+	.vsync_start = 2160 + 20,
+	.vsync_end = 2160 + 20 + 4,
+	.vtotal = 2160 + 20 + 4 + 28,
+	.width_mm = 68,
+	.height_mm = 136,
+};
+
+static const struct panel_desc_dsi nt36672_txd = {
+	.desc = {
+		.modes = &nt36672_txd_mode,
+		.num_modes = 1,
+		.bpc = 8,
+		.size = {
+			.width = 68,
+			.height = 136,
+		},
+		.connector_type = DRM_MODE_CONNECTOR_DSI,
+	},
+	.flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+		 MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_EOT_PACKET |
+		 MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM,
+	.format = MIPI_DSI_FMT_RGB888,
+	.lanes = 4,
+};
+
 static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "auo,b080uan01",
@@ -4673,6 +4705,9 @@ static const struct of_device_id dsi_of_match[] = {
 	{
 		.compatible = "mdss,td4310-txd",
 		.data = &td4310_txd
+        }, {
+		.compatible = "mdss,nt36672-txd",
+		.data = &nt36672_txd
 	}, {
 		/* sentinel */
 	}
