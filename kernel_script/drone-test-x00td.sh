8d9208a9fc6ce3a504662e0461fcf69c9631b2c2
iAboothahir <65064906+iAboothahir@users.noreply.github.com>
2021-09-29 12:31:21 +0530

    Update drone-test-x00td.sh

--- kernel_script/drone-test-x00td.sh
+++ kernel_script/drone-test-x00td.sh
@@ -178,7 +178,7 @@ exports() {
 	if [ $COMPILER = "gcc" ]
 	then
 		echo 'Compiling with gcc !'
-		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-linux-gnu-gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
 	fi
 
